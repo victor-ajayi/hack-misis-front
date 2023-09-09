f82ba618f042eff6bc0160b685c9966e2972f775
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-09 14:44:48 +0300

    feat: add homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,53 @@
 import Navbar from "../../components/Navbar";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <main className="main-container">
+        <div className="header">
+          <h1>
+            ITAM.<span id="clubs">clubs</span> — единая платформа для
+            организации <br />
+            студенческой проектной деятельности
+          </h1>
+        </div>
+
+        <button className="register">Загеристрироваться!</button>
+
+        <section className="clubs-container">
+          <h3>Познакомьтесь с сообществами</h3>
+          <h5>Выбирай, что нравится!</h5>
+
+          <div className="club-group">
+            <div className="club">
+              <p className="club--header">Хакатон клуб</p>
+              <p className="club--subheader"></p>
+            </div>
+            <div className="club">
+              <p className="club--header">Дизайнеры DAMN</p>
+              <p className="club--subheader"></p>
+            </div>
+            <div className="club">
+              <p className="club--header">Gamedev club</p>
+              <p className="club--subheader"></p>
+            </div>
+            <div className="club">
+              <p className="club--header">AI Knowledge Club</p>
+              <p className="club--subheader"></p>
+            </div>
+            <div className="club">
+              <p className="club--header">Робототехника</p>
+              <p className="club--subheader"></p>
+            </div>
+            <div className="club">
+              <p className="club--header">Курсы от ITAM</p>
+              <p className="club--subheader"></p>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
