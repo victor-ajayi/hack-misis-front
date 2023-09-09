b2e660e7006b31ce6b1d676c68a8a4a43eb4b80c
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-09 13:34:57 +0300

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import "./styles/Navbar.css";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <p>ITAM.hack</p>
-      <button className="profile">
-        <a href="">Личный кабинет</a>
-      </button>
+      <p>
+        <span className="itam">ITAM.</span>clubs
+      </p>
+      <Link to="/login">
+        <button className="profile">Личный кабинет</button>
+      </Link>
     </div>
   );
 }
