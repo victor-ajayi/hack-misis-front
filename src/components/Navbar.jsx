5876654ca60052757501ed81c4e9ea173e9bc633
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-09 16:46:37 +0300

    add link to home page in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./styles/Navbar.css";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/");
+  }
   return (
     <div className="navbar">
-      <p>
+      <p onClick={handleClick}>
         <span className="itam">ITAM.</span>clubs
       </p>
 
