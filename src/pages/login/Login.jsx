307dd7676a89a23d4d8fc8ca02e42f6c444d17ad
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-09 16:46:42 +0300

    update login css

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,47 @@
 import Navbar from "../../components/Navbar";
-import "../../components/styles/Login.css"
+import "../../components/styles/Login.css";
 export default function Login() {
-
   return (
     <div>
       <Navbar />
-      <div class="registration-cssave">
-    <form>
-        <h3 class="text-center">Вход в аккаунт</h3>
-        <div class="form-group">
-            <input class="form-control item" type="text" name="email" maxlength="15" minlength="4"
-             pattern="^[a-zA-Z0-9_.-]*$" id="email" placeholder="Email" 
-            required/>
-        </div>
-        <div class="form-group">
-            <input class="form-control item" type="password" name="Пароль" minlength="6" id="password" placeholder="Пароль" required/>
-        </div>
-        
-        <div class="form-group">
-            <button class="btn btn-primary btn-block create-account" type="submit">Войти</button>
-            <button class="btn btn-primary btn-block create-account" type="submit">зарегистрироваться</button>
-        </div>
-    </form>
-</div>
+      <div className="registration-cssave">
+        <form>
+          <h3 className="text-center">Вход в аккаунт</h3>
+          <div className="form-group">
+            <input
+              className="item"
+              type="text"
+              name="email"
+              maxlength="15"
+              minlength="4"
+              pattern="^[a-zA-Z0-9_.-]*$"
+              id="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="item"
+              type="password"
+              name="Пароль"
+              minlength="6"
+              id="password"
+              placeholder="Пароль"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="login" type="submit">
+              Войти
+            </button>
+            <button className="create-account" type="submit">
+              Зарегистрироваться
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
