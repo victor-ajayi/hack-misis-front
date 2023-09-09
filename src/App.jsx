437c255b275d43be7c8b0c7306ebbedf021503df
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-09 12:13:00 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>This is the main page</h1>
+    </>
+  );
+}
+
+export default App;
