801aeec431dd31946b657d235ca26cbeecd75bd6
Ravil <sergey_esenin1@mail.ru>
2023-09-11 12:11:48 +0300

    added dashboard

---
+++ src/pages/teamsearch/Teamsearch.jsx
@@ -0,0 +1,9 @@
+import "./Teamsearch.css"
+import Navbar from "../../components/Navbar";
+export default function Profile() {
+    return <div>
+        <Navbar/>
+        Вы авторизованы
+        </div>;
+  }
+  
